adff1e35d8b45ea5ecc69cf662952bd3082f1311
JustAgent <baykov.ivan.03@gmail.com>
2024-05-16 12:22:21 +0300

    settings

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,25 @@
 import "./Header.scss";
+import image from "../../assets/Settings.png";
+import { useNavigate } from "react-router-dom";
 
 export const Header = (props) => {
+  const navigate = useNavigate();
+
+  function nav() {
+    navigate("/settings");
+  }
   return (
-    <div className="headerWrapper" style={{justifyContent: `${props.text !== "WeWalk" && "center"}`, padding: `${props.text !== "WeWalk" && 0}` }}>
+    <div
+      className="headerWrapper"
+      style={{
+        justifyContent: `${props.text !== "WeWalk" && "center"}`,
+        padding: `${props.text !== "WeWalk" && 0}`,
+      }}
+    >
       <h1 className="headerTitle">{props.text}</h1>
+      <a onClick={nav}>
+        <img src={image} className="image-icon" />
+      </a>
     </div>
   );
 };
