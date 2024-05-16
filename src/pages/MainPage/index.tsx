79ab24149e2a49f2efacd5947ae2bc5215d5f33e
JustAgent <baykov.ivan.03@gmail.com>
2024-05-16 12:13:34 +0300

    routes

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -6,23 +6,19 @@ import image from "../../assets/Group 1.png";
 export const MainPage: React.FC = () => {
   return (
     <div className="mainPageContainer">
-      <Header text={"WeWalk"}/>
+      <Header text={"WeWalk"} />
       <div className="rectangle"></div>
       <div className="imageContainer">
-        <img src={image} className="image"/>
+        <img src={image} className="image" />
         <div className="infoContainer">
           <h2 className="title">My Interests</h2>
           <p>Rock-n-Roll music</p>
           <p>Dramatic films</p>
-          <p>Drawing</p>
-          <p>Nature</p>
+          {/* <p>Drawing</p>
+          <p>Nature</p> */}
         </div>
-        <button className="main__button">
-          Create walking route
-        </button>
-        <button className="main__button">
-          Search
-        </button>
+        <button className="main__button">Create walking route</button>
+        <button className="main__button">Search</button>
       </div>
     </div>
   );
--- src/pages/Routes/index.tsx
+++ src/pages/Routes/index.tsx
@@ -1,43 +1,81 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import "./RoutesPage.scss";
-import image from "../../assets/Group 1.png";
 import axios from "axios";
+import image from "../../assets/Map.png";
 
-interface RoutesResponse : {
-
-}
 export const RoutesPage: React.FC = () => {
+  interface RoutesResponse {
+    name: string;
+    description: string;
+    created_at: string;
+    id: number;
+    owner_id: number;
+  }
+  let names = [
+    "Justin",
+    "Ivan",
+    "Arina",
+    "Daniel",
+    "Michael",
+    "Ivan",
+    "Ivan",
+    "Ivan",
+    "Ivan",
+    "Ivan",
+    "Ivan",
+    "Ivan",
+  ];
+  let test = [
+    {
+      name: "Route 1",
+      description: "A long tour",
+      created_at: "2023-01-03",
+      id: 1,
+      owner_id: 1,
+    },
+    {
+      name: "Route 1",
+      description: "A long tour",
+      created_at: "2020-01-03",
+      id: 1,
+      owner_id: 1,
+    },
+  ];
+  const [routes, setroutes] = useState<Array<RoutesResponse>>(test);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://localhost:3000/routes");
-        const data: RoutesResponse = response.data;
-        localStorage.setItem("token", token);
+        const data: RoutesResponse[] = response.data;
+        // if (data) setroutes(data);
       } catch (error) {
-        console.error("Ошибка авторизации:", error);
+        console.error("Ошибка routes:", error);
       }
     };
 
     fetchData(); // Call the async function here
   }, []);
-
+  console.log(routes);
   return (
     <div className="mainPageContainer">
       <Header text={"Routes"} />
-      <div className="rectangle"></div>
-      <div className="imageContainer">
-        <img src={image} className="image" />
-        <div className="infoContainer">
-          <h2 className="title">My Interests</h2>
-          <p>Rock-n-Roll music</p>
-          <p>Dramatic films</p>
-          <p>Drawing</p>
-          <p>Nature</p>
-        </div>
-        <button className="main__button">Create walking route</button>
-        <button className="main__button">Search</button>
-      </div>
+
+      {routes.map((item) => {
+        return (
+          <div className="infoContainer">
+            <img src={image} className="image" alt="Item Image" />
+            <div className="itemDetails">
+              <p>#: {item.id}</p>
+              <p>
+                <b>{item.name}</b>
+              </p>
+              <p>{item.created_at}</p>
+              <p>{names[item.owner_id]}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
